1f547e7c94f3dac6bfdfb5fcce86ffef9a82cecd
leonarci <artem.janicki@gmail.com>
2022-06-12 23:34:00 +0300

    feat: add posibility to provide paths with spaces in single quoetes
    to use this method user must provide two paths (if command require
    two paths) in single quoetes

--- file-manager/io-interface/parseUserCommand.js
+++ file-manager/io-interface/parseUserCommand.js
@@ -1,14 +1,25 @@
 export const parseUserCommand = async (line) => {
   const lineTrimed = line.trim();
-  const regexpForSpacesInPath = /(?<![\\])\s/g;
-  const inputArray = lineTrimed.split(regexpForSpacesInPath);
+  let commandObject, command, argsParsed;
+  if (lineTrimed.includes("'")) {
+    command = lineTrimed.slice(0, lineTrimed.indexOf(' '));
+    const args = lineTrimed.slice(lineTrimed.indexOf(' ') + 1);
+    const arrgsArray = args.split("' '");
+    argsParsed = arrgsArray.map(arg => {
+      let argNormalized = arg.replaceAll("'", '');
+      return argNormalized;
+    });
+  } else {
+    const regexpForSpacesInPath = /(?<![\\])\s/g;
+    const inputArray = lineTrimed.split(regexpForSpacesInPath);
 
-  const command = inputArray.shift();
-  const args = inputArray;
+    command = inputArray.shift();
+    const args = inputArray;
 
-  const regexpForRemovingSapacesInPath = /\\(?=\s)/g;
-  const argsParsed = args.map(arg => { return arg.replaceAll(regexpForRemovingSapacesInPath, ''); });
-  const commandObject = {
+    const regexpForRemovingSapacesInPath = /\\(?=\s)/g;
+    argsParsed = args.map(arg => { return arg.replaceAll(regexpForRemovingSapacesInPath, ''); });
+  }
+  commandObject = {
     command,
     args: argsParsed
   };
