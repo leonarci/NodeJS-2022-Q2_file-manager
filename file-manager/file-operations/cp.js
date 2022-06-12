8f37e4e538ff3d34d6369807e7745bea7cfc7262
leonarci <artem.janicki@gmail.com>
2022-06-12 22:51:06 +0300

    feat: add posibility to cp or mv file if destination filename exists
    if destination filename exists '-copy' prefix will be added to file copy

--- file-manager/file-operations/cp.js
+++ file-manager/file-operations/cp.js
@@ -2,7 +2,7 @@ import createFsReadStream from '../utils/streams/createFsReadStream.js';
 import createFsWriteStream from '../utils/streams/createFsWriteStream.js';
 import getAbsolutePath from "../utils/path/getAbsolutePath.js";
 import { existFile } from "../utils/path/existFile.js";
-import { join, basename } from 'path';
+import { join, basename, extname } from 'path';
 import { stat } from 'fs/promises';
 import FileSystemError from '../errors/FileSystemError.js';
 
@@ -21,9 +21,12 @@ export const cp = async (userInputPaths) => {
 
       if (!pathToFileStats.isFile()) throw new FileSystemError('Operation failed: first argument should be type of file');
       if (!pathToFolderStats.isDirectory()) throw new FileSystemError('Operation failed: you should enter existed directory path as second argument');
-      const fileName = basename(pathToFile);
-      const pathToFileCopy = join(pathToFolder, fileName);
-      if (await existFile(pathToFileCopy)) throw new FileSystemError('Operation failed: file already exists in given directory');
+      const fileExtantion = extname(pathToFile);
+      const fileName = basename(pathToFile, fileExtantion);
+      let pathToFileCopy = join(pathToFolder, `${fileName}${fileExtantion}`);
+      while (await existFile(pathToFileCopy)) {
+        pathToFileCopy = join(pathToFolder, `${basename(pathToFileCopy, fileExtantion)}-copy${fileExtantion}`);
+      }
       const rs = await createFsReadStream(pathToFile);
       const ws = await createFsWriteStream(pathToFileCopy);
       rs.pipe(ws).on('error', error => rej(error));;
